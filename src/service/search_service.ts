8d3d0b4362677ad8a64594e8edc3b450556330ae
Otsuki Hitoshi <hitochan777@gmail.com>
2019-02-25 00:01:28 +0900

    Refine dashboard

--- src/service/search_service.ts
+++ src/service/search_service.ts
@@ -21,6 +21,9 @@ export class HttpSearchService implements SearchService {
 
 export class MockSearchService implements SearchService {
   public async search(query: string): Promise<Book[]> {
+    await new Promise( resolve => {
+      setTimeout( () => {resolve()}, 1000)
+    })
     const books = await Promise.resolve(mockData)
     // return books.filter(book => book.title.includes(query))
     return books
