40be703539e8f3987d6158400589ba05b8dea4d1
Otsuki Hitoshi <hitochan777@gmail.com>
2019-02-24 23:38:24 +0900

    Add book component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,31 +4,53 @@
       <input v-model="keyword" placeholder="Type something here to search!" />
       <button @click="search(keyword)">Search</button>
     </div>
-      <ul class="search-result">
-        <li v-for="book in books">
-          {{book.id}}
-          </li>
-        </ul>
+    <ul class="search-result">
+      <Book
+        v-for="book in books"
+        :key="book.id"
+        :authors="book.authors"
+        :title="book.title"
+        :thumbnail="book.thumbnail"
+        :publisher="book.publisher"
+        :infoLink="book.infoLink"
+      />
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import {Component, Inject, Vue} from 'vue-property-decorator';
+import { Component, Inject, Vue } from 'vue-property-decorator'
 import { SearchService } from '../service/search_service'
-import {Book} from '../book'
+import { Book as BookItem } from '../book'
+import Book from './Book.vue'
 
-@Component
+@Component({
+  components: {
+    Book,
+  },
+})
 export default class Search extends Vue {
-    books: Book[] = []
-    keyword: string = ""
-    @Inject() searchService!: SearchService
-    public async search(keyword: string) {
-      const books = await this.searchService.search(keyword)
-      this.books = books
-      console.log(books)
-    }
+  books: BookItem[] = []
+  keyword: string = ''
+  @Inject() searchService!: SearchService
+  public async search(keyword: string) {
+    const books = await this.searchService.search(keyword)
+    this.books = books
+    console.log(books)
+  }
 }
-
 </script>
 
-<style scoped></style>
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.search-result {
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  flex-basis: auto;
+}
+</style>
